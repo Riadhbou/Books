38a3e0fe8a4c3c5f7c1168c3d8cf56b1f15095ee
riadhbou <riadh.jeed@hotmail.fr>
2019-03-06 10:47:47 +0100

    refreshe cockies

--- application.py
+++ application.py
@@ -20,12 +20,12 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 
 # Ensure responses aren't cached
-@app.after_request
+"""@app.after_request
 def after_request(response):
     response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
     response.headers["Expires"] = 0
     response.headers["Pragma"] = "no-cache"
-    return response
+    return response"""
 
 
 # Configure session to use filesystem (instead of signed cookies)
